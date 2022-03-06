85161912dd573a9eaf2771763769e339d210edfb
yaoandw <yaoandw@163.com>
2022-03-06 10:07:49 +0800

    修改传递参数对象字段

--- src/main/java/cn/monitor4all/logRecord/context/LogRecordContextReactive.java
+++ src/main/java/cn/monitor4all/logRecord/context/LogRecordContextReactive.java
@@ -5,17 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Slf4j
 @Data
 public class LogRecordContextReactive {
 
     private String msg;
     private String userId;
-    private String username;
-    private String nickname;
+    private String tag;
 
     public static Mono<StandardEvaluationContext> getSpelContext() {
         return Mono.subscriberContext().map(context -> {
